5a79d64e3ca415af39d7491322778b0bf940a8b4
Hayim <xym6336@163.com>
2018-12-18 01:50:53 +0800

    update

---
+++ src/views/index.vue
@@ -0,0 +1,73 @@
+<style scoped>
+  .layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+
+  }
+  .layout-logo{
+    width: 150px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+    text-align:center;
+    line-height: 30px;
+    font-weight: bold;
+    color: aliceblue;
+  }
+  .layout-nav{
+    float: right;
+    margin: 0 auto;
+    margin-right: 20px;
+  }
+  .layout-footer-center{
+    text-align: center;
+  }
+</style>
+<template>
+  <div class="layout">
+    <Layout>
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-logo">
+            操作系统可视化
+          </div>
+          <div class="layout-nav">
+            <MenuItem name="main">
+              <Icon type="ios-construct"></Icon>
+              主页
+            </MenuItem>
+            <MenuItem name="about">
+              <Icon type="ios-information-circle-outline"></Icon>
+              关于
+            </MenuItem>
+          </div>
+        </Menu>
+      </Header>
+      <Content :style="{padding: '0 50px'}">
+        <Breadcrumb :style="{margin: '20px 0'}">
+          <BreadcrumbItem>Home</BreadcrumbItem>
+          <BreadcrumbItem>Components</BreadcrumbItem>
+          <BreadcrumbItem>Layout</BreadcrumbItem>
+        </Breadcrumb>
+        <Card>
+          <div style="min-height: 200px;">
+            Content
+          </div>
+        </Card>
+      </Content>
+      <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+    </Layout>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
