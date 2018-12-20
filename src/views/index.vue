5f1a225661146a4dc40fdadd898f68c204628439
Hayim <xym6336@163.com>
2018-12-20 12:53:05 +0800

    keep changes

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,35 +1,70 @@
 <style>
-  #mainview{
+  #mainview {
     margin: 30px 0 30px 0;
     height: 25cm;
   }
-  #upper{
+
+  #upper {
     margin: 0 0 20px 0;
     height: 60%;
   }
-  #codeview{
+
+  #codeview {
     border: aliceblue;
     float: left;
     width: 35%;
     height: 15cm;
   }
-  #codeselecter{
+
+  #codeselecter {
     float: left;
-    width: 25%;
+    width: 20%;
     height: 100%;
     outline: slategrey;
   }
-  #codecontent{
+
+  #codecontent {
     float: left;
-    width: 75%;
+    width: 80%;
     height: 100%;
+    overflow: scroll;
+    background-color: #dcdee2;
   }
-  #monitor{
+
+  #monitor {
     width: 65%;
     height: 100%;
     float: left;
+    text-align: center;
   }
-  .layout{
+
+  #register {
+    width: 100%;
+    height: 60%;
+    padding-left: 20%;
+  }
+
+  #c1 {
+    float: left;
+  }
+
+  #c2 {
+    float: left;
+  }
+
+  #c3 {
+    float: left;
+  }
+
+  #c4 {
+    float: left;
+  }
+
+  #c5 {
+    float: left;
+  }
+
+  .layout {
     border: 1px solid #d7dde4;
     background: #f5f7f9;
     position: relative;
@@ -37,7 +72,8 @@
     overflow: hidden;
 
   }
-  .layout-logo{
+
+  .layout-logo {
     width: 150px;
     height: 30px;
     background: #5b6270;
@@ -46,37 +82,33 @@
     position: relative;
     top: 15px;
     left: 20px;
-    text-align:center;
+    text-align: center;
     line-height: 30px;
     font-weight: bold;
     color: aliceblue;
   }
-  .layout-nav{
+
+  .layout-nav {
     float: right;
     margin: 0 auto;
     margin-right: 20px;
   }
-  .layout-footer-center{
+
+  .layout-footer-center {
     text-align: center;
     background-color: #515a6e;
     color: whitesmoke;
   }
+
   .noselect {
     -webkit-touch-callout: none; /* iOS Safari */
     -webkit-user-select: none; /* Safari */
     -khtml-user-select: none; /* Konqueror HTML */
     -moz-user-select: none; /* Firefox */
     -ms-user-select: none; /* Internet Explorer/Edge */
-    user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome and Opera */
-  }
-  pre{
-    background-color: #f5f7f9;
-    overflow: visible;
-  }
-  code{
-    background-color: #f5f7f9;
-    overflow: visible;
+    user-select: none;
+    /* Non-prefixed version, currently
+                                     supported by Chrome and Opera */
   }
 </style>
 <template>
@@ -107,32 +139,52 @@
         <div id="mainview">
           <div id="upper">
             <div id="codeview">
-              <div id="codeselecter">
-                  <Menu theme="dark" width="auto" style="height: 100%" class="noselect">
-                    <MenuItem name="bios">
-                      BIOS
-                    </MenuItem>
-                    <MenuItem name="bootsectcode" @on-select="getContent">
-                      bootsect.S
-                    </MenuItem>
-                    <MenuItem name="setupcode">
-                      setup.S
-                    </MenuItem>
-                    <MenuItem name="headcode">
-                      head.S
-                    </MenuItem>
-                    <MenuItem name="maincode">
-                      main.c
-                    </MenuItem>
-                  </Menu>
+              <div id="codeselecter" class="noselect">
+                <Menu theme="dark" width="auto" style="height: 100%">
+                  <MenuItem name="bios">
+                    BIOS
+                  </MenuItem>
+                  <MenuItem name="bootsectcode" @click.native="getContent('bootsect.S')">
+                    bootsect.S
+                  </MenuItem>
+                  <MenuItem name="setupcode" @click.native="getContent('setup.S')">
+                    setup.S
+                  </MenuItem>
+                  <MenuItem name="headcode" @click.native="getContent('head.S')">
+                    head.S
+                  </MenuItem>
+                  <MenuItem name="maincode">
+                    main.c
+                  </MenuItem>
+                </Menu>
               </div>
               <div id="codecontent">
-                <Card style="height: 100%; overflow: scroll;">
-                  <pre v-highlightjs><code class="assembly">{{contents.content}}</code></pre>
-                </Card>
+                <pre v-highlightjs="content"
+                     style="background-color: rgba(0, 0, 0, 0);color: #444444;overflow: visible; height: auto">
+                  <code class="assembly"
+                        style="background-color: rgba(0, 0, 0, 0);color: #444444;overflow: visible; height: auto"></code>
+                </pre>
               </div>
+
             </div>
             <div id="monitor">
+              <div id="register">
+                <div id="c1">
+                  <Table border stripe :columns="columns1" :data="data1"></Table>
+                </div>
+                <div id="c2">
+                  <Table border stripe :columns="columns2" :data="data2"></Table>
+                </div>
+                <div id="c3">
+                  <Table border stripe :columns="columns3" :data="data3"></Table>
+                </div>
+                <div id="c4">
+                  <Table border stripe :columns="columns4" :data="data4"></Table>
+                </div>
+                <div id="c5">
+                  <Table border stripe :columns="columns5" :data="data5"></Table>
+                </div>
+              </div>
 
             </div>
           </div>
@@ -151,17 +203,116 @@
 </template>
 <script>
   import datas from '@/assets/mock/data.json'
+
   export default {
-    data(){
-      return{
-        codes: datas.code,
-        contents:  datas.code[2]
+    data() {
+      return {
+        content: "",
+        columns1: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Value',
+            key: 'value'
+          }
+        ],
+        data1: [
+          {
+            name: 'R1',
+            value: '0'
+          },
+          {
+            name: 'R2',
+            value: '0'
+          }
+        ],
+        columns2: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Value',
+            key: 'value'
+          }
+        ],
+        data2: [
+          {
+            name: 'R1',
+            value: '0'
+          },
+          {
+            name: 'R2',
+            value: '0'
+          }
+        ],
+        columns3: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Value',
+            key: 'value'
+          }
+        ],
+        data3: [
+          {
+            name: 'R1',
+            value: '0'
+          },
+          {
+            name: 'R2',
+            value: '0'
+          }
+        ],
+        columns4: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Value',
+            key: 'value'
+          }
+        ],
+        data4: [
+          {
+            name: 'R1',
+            value: '0'
+          },
+          {
+            name: 'R2',
+            value: '0'
+          }
+        ],
+        columns5: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Value',
+            key: 'value'
+          }
+        ],
+        data5: [
+          {
+            name: 'R1',
+            value: '0'
+          },
+          {
+            name: 'R2',
+            value: '0'
+          }
+        ]
       }
     },
-
     methods: {
-      getContent: function(name){
-        this.contents = this.codes[1]
+      getContent(name) {
+        this.content = datas.code.find(e => e.name === name).content;
       }
     }
   }
